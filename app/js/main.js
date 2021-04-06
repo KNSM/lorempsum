48316f5ae9d5835a79995026222acc515a7702e6
KNSM <knsmrt5k@gmail.com>
2021-04-06 17:00:56 +0300

    Добавил и адаптировал хедер, создал общую структуру страницы.
    Добавил все картинки, шрифты, иконки, начальные свойства элементов.

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
-console.log(1233332121333);
+$(document).ready(function () {
+    var ov = $('.ov');
+
+    //header menu
+    $('.header__menu').click(function () {
+        $('.header').toggleClass('-menu-opened -window-active');
+        ov.toggleClass('-window-active');
+    });
+
+    //header-nav-mobile
+    $('.header__nav .list__item.has-sublist .list__item-link').click(function () {
+        $(this).parent().find('.sublist-block').slideToggle();
+        $(this).parent().toggleClass('-active');
+    });
+
+    //ov
+    ov.click(function () {
+        $('.-menu-opened').removeClass('-menu-opened');
+        $('.-window-active').removeClass('-window-active');
+        $(this).removeClass('-window-active');
+    });
+});
