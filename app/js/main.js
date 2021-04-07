e22fadd9b96e29c4115c571b290aeedd35c7b904
KNSM <knsmrt5k@gmail.com>
2021-04-07 17:00:24 +0300

    Добавил sidebar и калькулятор, обновил скрипт selectora, добавил noUIslider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,100 @@
 $(document).ready(function () {
     var ov = $('.ov');
 
+    //fakeScroll
+    var fakeScrolls = document.querySelectorAll('.selector-menu'), fakeScroll;
+    for (fakeScroll = 0; fakeScroll < fakeScrolls.length; fakeScroll++) {
+        fakeScrolls[fakeScroll].fakeScroll();
+    }
+
+    // form-calc slider
+    $(function () {
+        var stepsSlider = document.getElementById('calc-slider');
+        var input0 = document.getElementById('input-calc-1');
+        var input1 = document.getElementById('input-calc-2');
+        var inputs = [input0, input1];
+
+        noUiSlider.create(stepsSlider, {
+            start: [0, 200000],
+            connect: true,
+            range: {
+                'min': [0],
+                'max': 20000
+            },
+            step: 500,
+            format: wNumb({
+                decimals: 0,
+            })
+        });
+
+        stepsSlider.noUiSlider.on('update', function (values, handle) {
+            inputs[handle].value = values[handle];
+        });
+
+        // Listen to keydown events on the input field.
+        inputs.forEach(function (input, handle) {
+
+            input.addEventListener('change', function () {
+                stepsSlider.noUiSlider.setHandle(handle, this.value);
+            });
+
+            input.addEventListener('keydown', function (e) {
+
+                var values = stepsSlider.noUiSlider.get();
+                var value = Number(values[handle]);
+
+                // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+                var steps = stepsSlider.noUiSlider.steps();
+
+                // [down, up]
+                var step = steps[handle];
+
+                var position;
+
+                // 13 is enter,
+                // 38 is key up,
+                // 40 is key down.
+                switch (e.which) {
+
+                    case 13:
+                        stepsSlider.noUiSlider.setHandle(handle, this.value);
+                        break;
+
+                    case 38:
+
+                        // Get step to go increase slider value (up)
+                        position = step[1];
+
+                        // false = no step is set
+                        if (position === false) {
+                            position = 1;
+                        }
+
+                        // null = edge of slider
+                        if (position !== null) {
+                            stepsSlider.noUiSlider.setHandle(handle, value + position);
+                        }
+
+                        break;
+
+                    case 40:
+
+                        position = step[0];
+
+                        if (position === false) {
+                            position = 1;
+                        }
+
+                        if (position !== null) {
+                            stepsSlider.noUiSlider.setHandle(handle, value - position);
+                        }
+
+                        break;
+                }
+            });
+        });
+    });
+
     //header menu
     $('.header__menu').click(function () {
         $('.header').toggleClass('-menu-opened -window-active');
@@ -19,7 +113,7 @@ $(document).ready(function () {
         var scrollTop = $(window).scrollTop(),
             header = $('.header');
 
-        if (scrollTop >  0) {
+        if (scrollTop > 0) {
             header.addClass('header-fixed');
         } else {
             header.removeClass('header-fixed');
@@ -31,5 +125,71 @@ $(document).ready(function () {
         $('.-menu-opened').removeClass('-menu-opened');
         $('.-window-active').removeClass('-window-active');
         $(this).removeClass('-window-active');
+    });
+
+    //selector
+    $(function () {
+        var selector = $('.selector');
+
+        selector.each(function () {
+
+            var select = $(this).find('.select'),
+                selectorMenu = $(this).find('.selector-menu'),
+                selectorMenuInner = selectorMenu.find('.menu'),
+                selectItems = select.find('option');
+
+            $(this).append('<div class="selector-block"><button type="button" class="button"></button><div class="tags"><button type="button" class="tag"></button></div></div>');
+            select.wrap('<div class="form-data"></div>');
+
+
+            var selectorBlock = $(this).find('.selector-block'),
+                selectorTag = selectorBlock.find('.tag'),
+                selectorButton = selectorBlock.find('.button');
+
+            selectItems.each(function () {
+
+                if ($(this).attr('selected')) {
+                    selectorMenuInner.append('<li data-id="' + $(this).attr('value') + '" class="selector-menu__item selected">' + $(this).html() + '</li>');
+
+                    var selectorMenuItem = selectorMenuInner.find('.selector-menu__item.selected');
+
+                    selectorTag.html($(this).html());
+                    selectorTag.attr('data-id', selectorMenuItem.attr('data-id'));
+                } else {
+                    selectorMenuInner.append('<li data-id="' + $(this).attr('value') + '" class="selector-menu__item">' + $(this).html() + '</li>');
+                }
+            });
+
+            var selectorMenuItem = selectorMenuInner.find('.selector-menu__item');
+
+            selectorButton.click(function () {
+                var menuLength = selectorMenuInner.find('.selector-menu__item').length;
+
+                if (menuLength < 5) {
+                    selectorMenu.css('height', 42 * menuLength + 12);
+                }
+
+                selectorMenu.toggleClass('-open');
+            });
+
+            selectorMenuItem.click(function () {
+                var dataId = $(this).attr('data-id'),
+                    selectorText = $(this).html();
+
+                selectItems.removeAttr('selected');
+
+                selectItems.each(function () {
+                    if ($(this)[0].value === dataId) {
+                        $(this).attr('selected', 'selected');
+                    }
+                });
+                selectItems.find('[value]', dataId);
+                selectorTag.html(selectorText);
+                selectorTag.attr('data-id', dataId);
+                selectorMenuItem.removeClass('selected');
+                $(this).addClass('selected');
+                selectorMenu.removeClass('-open');
+            });
+        });
     });
 });
